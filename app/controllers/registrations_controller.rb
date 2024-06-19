86b514eabc72c2e3d79cebc191679502386a4f81
Andrew Cottage <andcott@gmail.com>
2024-06-19 08:53:09 -0400

    allow registrations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,5 +3,22 @@ class RegistrationsController < ApplicationController
   end
 
   def create
+    user = User.new(registration_params)
+
+    if user.save
+      session[:user_id] = user.id
+
+      redirect_to root_path, notice: 'Logged in!'
+    else
+      flash.now[:alert] = 'Invalid email/password combination'
+      render 'new'
+    end
+
+  end
+
+  private
+
+  def registration_params
+    params.require(:registration).permit(:email, :password, :password_confirmation).merge(admin: false)
   end
 end
