6ecae3de89cbf408d39cfe5b629c9453b193f84b
Andrew Cottage <andcott@gmail.com>
2024-06-19 08:46:32 -0400

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :sessions, only: %i[new create destroy]
   resources :recipes, param: :slug
   resources :categories, param: :slug
+  resources :registrations, only: %i[new create]
   
     # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
