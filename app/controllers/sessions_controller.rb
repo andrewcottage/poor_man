db0a0eb4c033986cb00b01d6d91fb01766a15f39
Andrew Cottage <andcott@gmail.com>
2024-06-19 08:17:18 -0400

    sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
-    redirect_to root_url
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Logged out!'
   end
 end
