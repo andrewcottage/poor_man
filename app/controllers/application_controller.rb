58c2a02a499e5207d68ee1bcd49a2bd4e429a4b3
Andrew Cottage <andcott@gmail.com>
2024-12-30 18:56:53 -0500

    return path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,19 @@ class ApplicationController < ActionController::Base
   private
 
   def require_admin!
-    redirect_to root_path, alert: 'You are not authorized to access this page' unless Current.user&.admin&.present?
+    return unless Current.user&.admin&.present?
+
+    redirect_to new_session_path(
+      return_to: request.fullpath
+    ), alert: 'You are not authorized to access this page' 
   end
 
   def require_user!
-    redirect_to new_session_path, alert: 'You must be logged in to access this page' if Current.user.nil?
+    return unless Current.user.present?
+
+    redirect_to new_session_path(
+      return_to: request.fullpath
+    ), alert: 'You must be logged in to access this page'
   end
 
   def set_current_user
