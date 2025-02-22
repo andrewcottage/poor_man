31a815acbfb42fec6d98d74014c14d284e571261
Andrew Cottage <andcott@gmail.com>
2025-02-22 14:00:49 -0500

    Expire session after 1 month

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.cache_store = :solid_cache_store
 
   # Use a solid cache store for session storage.
-  config.session_store :cache_store, key: '_poor_man_session'
+  config.session_store :cache_store, key: '_poor_man_session', expires_after: 1.month
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
