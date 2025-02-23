ecf1465de86cff90202928c9535667933c5768f8
Andrew Cottage <andcott@gmail.com>
2025-02-22 19:34:36 -0500

    Fix OpenAI configuration to safely access credentials

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,4 +1,4 @@
 OpenAI.configure do |config|
-  config.access_token = Rails.application.credentials.open_ai.secret
+  config.access_token = Rails.application.credentials&.open_ai&.secret
   config.log_errors = Rails.env.development?
 end
