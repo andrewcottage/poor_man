ff0168aa2ed95c66362e879799ae509185531bcd
Andrew Cottage <andcott@gmail.com>
2025-03-30 13:16:45 -0400

    wip

--- test/mailers/recap_mailer_test.rb
+++ test/mailers/recap_mailer_test.rb
@@ -2,26 +2,21 @@ require 'test_helper'
 
 class RecapMailerTest < ActionMailer::TestCase
   test "monthly_recap" do
-    user = users(:one)  # Assuming you have a user fixture
-    
-    # Create some test data for the past month
+    user = users(:andrew)
     start_date = 1.month.ago.beginning_of_month
-    end_date = 1.month.ago.end_of_month
+    recipe = user.recipes.first
     
-    # Create test recipes and interactions
-    recipe = Recipe.create!(
-      user: user,
-      title: "Test Recipe"
+    recipe.favorites.create!(user: users(:victoria))
+    recipe.ratings.create!(
+      user: users(:victoria), 
+      title: "Great recipe!", 
+      comment: "Great recipe!", 
+      value: 4
     )
-    
-    3.times { recipe.favorites.create!(user: users(:two)) }
-    recipe.comments.create!(user: users(:two), content: "Great recipe!")
-    recipe.ratings.create!(user: users(:two), value: 4)
 
-    # Generate the email
+
     email = RecapMailer.monthly_recap(user)
 
-    # Assert the email properties
     assert_emails 1 do
       email.deliver_now
     end
@@ -29,9 +24,8 @@ class RecapMailerTest < ActionMailer::TestCase
     assert_equal [user.email], email.to
     assert_equal "Your Monthly Recipe Recap for #{start_date.strftime('%B %Y')}", email.subject
     
-    # Test email content
     assert_match "Hi #{user.name}", email.body.to_s
-    assert_match "3 new favorites", email.body.to_s
+    assert_match "1 new favorites", email.body.to_s
     assert_match "1 comments", email.body.to_s
     assert_match "4.0 stars", email.body.to_s
   end
